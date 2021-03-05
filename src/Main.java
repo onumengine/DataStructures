aaa61bc90a239b612dd6d5a13cec4208c173eaf6
onumengine <onumdev@gmail.com>
2021-03-05 14:39:19 +0100

    Tested the indexOf method. Test passed

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,8 @@ public class Main {
         System.out.println(customLinkedList.contains(12));
         System.out.println(customLinkedList.contains(23));
         System.out.println(customLinkedList.contains(43));
+        System.out.println(customLinkedList.indexOf(34));
+        System.out.println(customLinkedList.indexOf(23));
+        System.out.println(customLinkedList.indexOf(43));
     }
 }
